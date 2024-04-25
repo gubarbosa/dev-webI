53958f05565b9cd08e2378dd1959d4d74067dd07
gubarbosa <gugabarbosa.bsi@gmail.com>
2024-04-25 18:02:35 -0300

    adicionando descrição aos produtos

--- intro-to-vue-3/main.js
+++ intro-to-vue-3/main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots'
+            product: 'Boots',
+            description: 'Elevate your style with our Sleek Leather Ankle Boots. Made from high-quality leather, these boots offer both sophistication and durability. Featuring a modern design with a rounded toe and block heel, they effortlessly complement any outfit. With a cushioned insole and easy side zipper closure, these boots combine comfort and convenience for all-day wear. Upgrade your wardrobe with these versatile boots that exude timeless elegance.',
+            product2: 'Socks',
+            description2: 'Indulge in comfort with our Cozy Knit Crew Socks. Crafted from soft, breathable knit fabric, these socks offer warmth and coziness with every step. Their versatile crew length makes them perfect for pairing with sneakers or boots. Whether you are lounging at home or out for a casual stroll, these socks provide all-day comfort. Elevate your sock game with this essential accessory for any wardrobe.'
         }
     }
 })
