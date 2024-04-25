1f4209bfa6dc575b64746c7aae2fd6806cfd477f
gubarbosa <gugabarbosa.bsi@gmail.com>
2024-04-25 18:38:11 -0300

    adicionando url no site

--- intro-to-vue-3/main.js
+++ intro-to-vue-3/main.js
@@ -1,10 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots',
-            description: 'Elevate your style with our Sleek Leather Ankle Boots. Made from high-quality leather, these boots offer both sophistication and durability. Featuring a modern design with a rounded toe and block heel, they effortlessly complement any outfit. With a cushioned insole and easy side zipper closure, these boots combine comfort and convenience for all-day wear. Upgrade your wardrobe with these versatile boots that exude timeless elegance.',
-            product2: 'Socks',
-            description2: 'Indulge in comfort with our Cozy Knit Crew Socks. Crafted from soft, breathable knit fabric, these socks offer warmth and coziness with every step. Their versatile crew length makes them perfect for pairing with sneakers or boots. Whether you are lounging at home or out for a casual stroll, these socks provide all-day comfort. Elevate your sock game with this essential accessory for any wardrobe.'
+            url: 'https://www.vuemastery.com/',
+            product: 'Socks',
+            description: 'Indulge in comfort with our Cozy Knit Crew Socks. Crafted from soft, breathable knit fabric, these socks offer warmth and coziness with every step. Their versatile crew length makes them perfect for pairing with sneakers or boots. Whether you are lounging at home or out for a casual stroll, these socks provide all-day comfort. Elevate your sock game with this essential accessory for any wardrobe.',
+            image: './assets/images/socks_blue.jpg',
+            product2: 'Boots',
+            description2: 'Elevate your style with our Sleek Leather Ankle Boots. Made from high-quality leather, these boots offer both sophistication and durability. Featuring a modern design with a rounded toe and block heel, they effortlessly complement any outfit. With a cushioned insole and easy side zipper closure, these boots combine comfort and convenience for all-day wear. Upgrade your wardrobe with these versatile boots that exude timeless elegance.',
         }
     }
 })
