3042e45a1fc4968e1553dda2242cf5e955e1655c
gubarbosa <gugabarbosa.bsi@gmail.com>
2024-07-10 16:37:28 -0300

    aula 08 - propriedades calculadas

---
+++ Aula 08 - Propriedades Calculadas/main.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+            onSale: false,
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            selectedVariant: 0,
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeToCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        available() {
+            return this.brand + ' ' + this.product + ' ' + 'is on sale'
+        }
+    }
+})
