75e1a69979292c704c86b177b01aea57f8da54e8
gubarbosa <gugabarbosa.bsi@gmail.com>
2024-08-02 09:32:08 -0300

    roteamento dinâmico

--- vue_3_mundo_real/real-world-vue/src/components/EventCard.vue
+++ vue_3_mundo_real/real-world-vue/src/components/EventCard.vue
@@ -10,13 +10,19 @@ defineProps({
 </script>
 
 <template>
-  <div class="event-card">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-  </div>
+  <RouterLink
+    class="event-link"
+    :to="{ name: 'event-details', params: { id: event.id } }"
+  >
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
+
 .event-card {
   padding: 20px;
   width: 250px;
@@ -28,4 +34,9 @@ defineProps({
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
+}
+
 </style>
--- vue_3_mundo_real/real-world-vue/src/router/index.js
+++ vue_3_mundo_real/real-world-vue/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: EventListView
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView,
     },
     {
       path: '/about',
